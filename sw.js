d77706a98d578f72f6855fcab1a7d668ed537907
Alzoz coder X <za6243247@gmail.com>
2026-06-10 01:52:52 +0300

    Update sw.js

---
+++ sw.js
@@ -0,0 +1,35 @@
+const CACHE = 'aura-v1';
+const STATIC = [
+  '/followstu/',
+  '/followstu/index.html'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(STATIC))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => {
+      if (cached) return cached;
+      return fetch(e.request).then(res => {
+        if (!res || res.status !== 200 || res.type === 'opaque') return res;
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      }).catch(() => caches.match('/followstu/index.html'));
+    })
+  );
+});
